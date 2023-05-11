c019a046172b27e27f8e746beab8c83d4b193902
María Isabel González <83318856+gm2021g@users.noreply.github.com>
2023-05-10 22:26:25 -0300

    Delete Desafío_3/english-univ/src directory

--- Desafío_3/english-univ/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Input } from "./input/index";
-export { default as CustomModal } from "./modal/index";
-export { default as Header } from "./header/index";
-export { default as Detail } from "./detail/index";
-export { default as Courses } from "./courses/index";
-
