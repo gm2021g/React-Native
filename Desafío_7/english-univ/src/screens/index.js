078c8d6de3f028f511daa861e23e81429d97eb53
María Isabel González <83318856+gm2021g@users.noreply.github.com>
2023-06-14 13:45:57 -0300

    Update index.js

--- Desafío_7/english-univ/src/screens/index.js
+++ Desafío_7/english-univ/src/screens/index.js
@@ -9,4 +9,4 @@ export { default as Profile } from "./profile/index";
 export { default as MapsScreen } from "./maps";
 export { default as NewPlaceScreen } from "./new-place";
 export { default as PlaceDetailScreen } from "./place-detail";
-export { default as PlaceListScreen } from "./place-list";
+
